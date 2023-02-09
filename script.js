793e8018bd95772598d3482ad7536a0ad0d45407
Damien <damien.deboudt@sfr.fr>
2023-02-09 17:31:23 +0100

    front ok.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-//recup boutton
 const rock = document.getElementById("Pierre");
 const scissor = document.getElementById("Ciseaux");
 const paper = document.getElementById("Feuille");
 
-//affichage score
+//screen score
 let player = document.getElementById("score-joueur");
 let ia = document.getElementById("score-IA");
+let roundResult = document.getElementById("round-result");
 
 //Player score and IA score = 0
 let playerScore = 0;
@@ -30,24 +30,52 @@ let game = (player_choice) => {
 
     //egalité
     if(player_choice === ia_choice) {
-        alert("égalité")
+        roundResult.innerText = "Egalité"
+        roundResult.style.color = "black"
     }
     //win
     else if ( player_choice  === 0 && ia_choice === 2
             || player_choice === 1 && ia_choice === 0
             || player_choice === 2 && ia_choice === 1) {
-        alert("Gagné, l'IA avait choisit " + ia_choice)
-        playerScore++
-        player.innerText = "Score joueur = " + playerScore
+        if (ia_choice === 0) {
+            roundResult.innerText = "Gagné, l'IA avait choisit Pierre"
+            roundResult.style.color = "green"
+            playerScore++
+            player.innerText = "Score joueur = " + playerScore;
+        } else if (ia_choice === 1) {
+            roundResult.innerText = "Gagné, l'IA avait choisit Feuille"
+            roundResult.style.color = "green"
+            playerScore++
+            playerScore.innerText = "Score joueur = " + playerScore;
+        } else {
+            roundResult.innerText = "Gagné, l'IA avait choisit Ciseaux"
+            roundResult.style.color = "green"
+            playerScore++
+            player.innerText = "Score joueur = " + playerScore;
+        }
     }
+
     //loose
     else if ( player_choice  === 2 && ia_choice === 0
             || player_choice === 0 && ia_choice === 1
-            || player_choice === 1 && ia_choice === 2)
-    {
-        alert("Perdu, l'IA avait choisit " + ia_choice)
-        iaScore++
-        ia.innerText = "Score IA = " + iaScore;
+            || player_choice === 1 && ia_choice === 2) {
+                if (ia_choice === 0) {
+                    roundResult.innerText = "Perdu, l'IA avait choisit Pierre"
+                    roundResult.style.color = "red"
+                    iaScore++
+                    ia.innerText = "Score IA = " + iaScore;
+                } else if (ia_choice === 1) {
+                    iaScore++
+                    ia.innerText = "Score IA = " + iaScore;
+                    roundResult.innerText = "Perdu, l'IA avait choisit Feuille"
+                    roundResult.style.color = "red"
+                } else {
+                    iaScore++
+                    ia.innerText = "Score IA = " + iaScore;
+                    roundResult.innerText = "Perdu, l'IA avait choisit Ciseaux"
+                    roundResult.style.color = "red"
+                }
+
     }
 
     if (playerScore === 10) {
@@ -56,11 +84,13 @@ let game = (player_choice) => {
         iaScore = 0;
         player.innerText = "Score joueur = " + playerScore
         ia.innerText = "Score IA = " + iaScore;
+        roundResult.innerText = ""
     } else if ( iaScore === 10) {
         alert("Vous avez perdu contre un Ordinateur");
         iaScore = 0;
         playerScore = 0;
-        player.innerText = "Score joueur = " + playerScore
-        ia.innerText = "Score IA = " + iaScore;
+        player.innerText = "Score joueur = "
+        ia.innerText = "Score IA = "
+        roundResult.innerText = ""
     }
 };
